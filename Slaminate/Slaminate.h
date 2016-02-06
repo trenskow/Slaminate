e73a525cc9a353a10db9f0a6a2f6825383e6c7bb
Kristian Trenskow <trenskow@me.com>
2016-02-06 16:47:33 +0100

    Initial Commit

---
+++ Slaminate/Slaminate.h
@@ -0,0 +1,19 @@
+//
+//  Slaminate.h
+//  Slaminate
+//
+//  Created by Kristian Trenskow on 06/02/16.
+//  Copyright © 2016 Trenskow.io. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Slaminate.
+FOUNDATION_EXPORT double SlaminateVersionNumber;
+
+//! Project version string for Slaminate.
+FOUNDATION_EXPORT const unsigned char SlaminateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Slaminate/PublicHeader.h>
+
+
