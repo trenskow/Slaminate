cbbf7836bf2c13dce171ba6b8930177d5e2b74a4
Kristian Trenskow <trenskow@me.com>
2016-02-13 02:59:04 +0100

    Implemented scrobbing.

---
+++ Slaminate/Array.swift
@@ -0,0 +1,9 @@
+//
+//  Array.swift
+//  Slaminate
+//
+//  Created by Kristian Trenskow on 12/02/16.
+//  Copyright © 2016 Trenskow.io. All rights reserved.
+//
+
+import Foundation
---
+++ Slaminate/ConstraintConstantAnimation.swift
@@ -0,0 +1,9 @@
+//
+//  ConstraintConstantAnimation.swift
+//  Slaminate
+//
+//  Created by Kristian Trenskow on 12/02/16.
+//  Copyright © 2016 Trenskow.io. All rights reserved.
+//
+
+import Foundation
